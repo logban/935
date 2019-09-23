2046ca15abbc7afbaa251b52f54fa2be9642a918
logban <heysmith6316@docomo.ne.jp>
2019-09-23 15:51:10 +0900

    komentofo-munotuika

--- articles/urls.py
+++ articles/urls.py
@@ -5,12 +5,14 @@ from .views import (
     ArticleDeleteView,
     ArticleDetailView,
     ArticleCreateView,
+    ArticleCommentView,
 )
 
 urlpatterns = [
     path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
     path('<int:pk>/',ArticleDetailView.as_view(), name='article_detail'),
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('<int:pk>/comment/', ArticleCommentView.as_view(), name='article_comment'),
     path('new/', ArticleCreateView.as_view(),name='article_new'),
     path('', ArticleListView.as_view(), name = 'article_list'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,29 +1,52 @@
 from django.shortcuts import render
-
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
-from .models import Article
+from .models import Article, Comment
 from django.urls import reverse_lazy
-class ArticleListView(ListView):
+
+class ArticleListView(LoginRequiredMixin,ListView):
     model = Article
     template_name = 'article_list.html'
+    login_url = 'login'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin,DetailView):
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
+    login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Article
     template_name  = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class ArticleCommentView(generic.CreateView):
+    model = Comment
+    template_name = 'article_comment.html'
+    fields = ('article','comment', 'author')
